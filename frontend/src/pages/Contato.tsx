eda047b0a58bb903af4171620c1227a96dd27d25
Marlon-DH <hermannmarlon7@gmail.com>
2026-09-01 11:38:47 -0300

    feat: implementar página de Produtos com funcionalidades de cadastro, edição e exclusão de usuários

--- frontend/src/pages/Contato.tsx
+++ frontend/src/pages/Contato.tsx
@@ -1,5 +1,5 @@
 function Contato() {
-  return <h1 className="text-5xl font-bold text-blue-800">Contato</h1>;
+  return <h1 className="text-5xl font-bold text-blue-800 ">Contato</h1>;
 }
 
 export default Contato;
