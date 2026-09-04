c9252d5a380be4ebc7c5ca6f0c25b404fa99dae3
Marlon-DH <hermannmarlon7@gmail.com>
2026-09-04 11:48:26 -0300

    feat: adicionar funcionalidade de busca e exibição de contatos na página Contato

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,24 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
+app.use(cors());
 const port = 3000;
 
-const contact = [
-    {id: 1, nome: "Bosta Liquida", email: "bostaliquida123@gmail.com"},
-    {id: 2, nome: "Pitbull Samba", email: "pitbulldosamba1@gmail.com"},
-    {id: 3, nome: "Mauro Shampu", email: "ibisfc@gmail.com"},
+const contatos = [
+  { id: 1, nome: "Mauro", email: "canivete@teste.com" },
+  { id: 2, nome: "Samuel", email: "samuelemauro@teste.com" },
+  { id: 3, nome: "Gu", email: "passeio@teste.com" },
 ];
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/contatos', (req: Request, res: Response) => {
-  res.send(contact);
+app.get("/api/contatos", (req: Request, res: Response) => {
+  res.json(contatos);
 });
 
 app.listen(port, () => {
-  console.log(`Servidor inciado em: http://localhost:${port}`);
+  console.log(`Servidor iniciado em: http://localhost:${port}`);
 });
